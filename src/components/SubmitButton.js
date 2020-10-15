9bc63947d2021fea6760b7fdd3ee7405b7a942a0
Cleiton Pipino Queiroz <cleitonpqz@gmail.com>
2020-10-15 01:54:05 -0300

    button implemented

---
+++ src/components/SubmitButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubmitButton = () => (
+  <div>
+    <button className="button" type="submit">
+      Search
+    </button>
+  </div>
+);
+
+export default SubmitButton;
